fe59ac92c8eb8be7db65f30c0fd4dfcc9bc0aa98
Romell D.Z. 福笛 <romllz489@gmail.com>
2020-10-19 23:27:13 -0500

    add all changing files

--- pre_data_augmentation.py
+++ pre_data_augmentation.py
@@ -11,6 +11,7 @@ point = '.'
 extension = 'jpg'
 point_extension = '.'+ extension
 tag = '_data_au{}_'
+import random
 
 #%%
 def preview(image):
@@ -23,10 +24,43 @@ def insensitive_glob(pattern):
     return glob.glob(''.join(map(either, pattern)))
 
 #%%
+def build_model(input_shape): 
+    from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+    from keras.models import Sequential
+    from keras import optimizers
+
+    model = Sequential()
+
+    model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', 
+                    input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                optimizer=optimizers.RMSprop(lr=1e-4),
+                metrics=['accuracy'])
+    return model
+
+#%%
 def main():
     list_images = []
     IMG_DIM = (640, 480)
-    for filename in insensitive_glob(os.path.join('data','*','*.{}').format(extension)): 
+    for filename in insensitive_glob(os.path.join('images','*','*.{}').format(extension)): 
         if '.DS_Store' in filename or '_' in filename:
             continue
         list_images.append(filename)
@@ -34,7 +68,7 @@ def main():
                                    width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2, 
                                    horizontal_flip=True, fill_mode='nearest')
 
-    print(list_images)
+    # print(list_images)
     # val_datagen = ImageDataGenerator(rescale=1./255)
     # validation_imgs_scaled  = validation_imgs.astype('float32')
     # validation_imgs_scaled /= 255
@@ -44,18 +78,18 @@ def main():
     train_imgs_scaled = train_imgs.astype('float32')
     train_imgs_scaled /= 255
 
+    train_labels =[random.choice(['100','50','10','20']) for i in train_imgs]
 
-    train_labels =['100','50','10','20']
-    print(train_imgs.shape)
-
-    bill_generator = train_datagen.flow(train_imgs, train_labels,
-                                   batch_size=1)
+    bill_generator = train_datagen.flow(train_imgs, train_labels, batch_size=1)
     bill =  [next(bill_generator) for i in range(0,5)]
     fig, ax = plt.subplots(1,5, figsize=(15, 6))
-    print('Labels:', [item[1][0] for item in bill])
-    l = [ax[i].imshow(bill[i][0][0]),ax.set_yticks([]), plt.set_xticks([]) for i in range(0,5)]
-    
+    for i in range(0,5):
+        ax[i].imshow(bill[i][0][0])
+        plt.show()
+        # ax.set_yticks([])
+        # ax.set_xticks([])
                   
+    model = build_model((640, 480,3))
     history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=100,
                                 validation_data=val_generator, validation_steps=50, 
                                 verbose=1) 
